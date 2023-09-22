5a6e80efed10b8149e9098074e921cb69b963437
Riad <riad@web-vision.de>
2023-09-22 16:02:10 +0200

    [TASK] Fix template paths

---
+++ packages/fgtclb/academic-bite-jobs/Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('TYPO3') || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'academic_bite_jobs',
+    'Configuration/TypoScript',
+    'Academic Bite Jobs'
+);
