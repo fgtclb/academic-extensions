c0b93a85032a21968224ad96c7cff9052b7722fc
Philipp Halle <p.halle@web-vision.de>
2025-01-14 11:28:47 +0100

    [TASK] Apply changed access to the page ID for TYPO3 v13 and higher

--- packages/fgtclb/academic-contact4pages/Classes/DataProcessing/ContactsProcessor.php
+++ packages/fgtclb/academic-contact4pages/Classes/DataProcessing/ContactsProcessor.php
@@ -3,6 +3,7 @@
 namespace FGTCLB\AcademicContacts4pages\DataProcessing;
 
 use FGTCLB\AcademicContacts4pages\Domain\Repository\ContactRepository;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
@@ -25,9 +26,19 @@ class ContactsProcessor implements DataProcessorInterface
         array $processedData
     ) {
         $contactRepository = GeneralUtility::makeInstance(ContactRepository::class);
-        $contacts = $contactRepository->findByPid($processedData['data']['uid']);
 
-        $processedData['contacts'] = $contacts;
+        // With the introduction of the new PageViewContentObject in version TYPO3 v13.1 the structure of the processedData array has changed.
+        // @see https://docs.typo3.org/c/typo3/cms-core/main/en-us//Changelog/13.1/Feature-103504-NewContentObjectPageView.html
+        // @see https://github.com/TYPO3/typo3/blob/12.4/typo3/sysext/frontend/Classes/ContentObject/FluidTemplateContentObject.php#L283
+        // @see https://github.com/TYPO3/typo3/blob/13.1/typo3/sysext/frontend/Classes/ContentObject/PageViewContentObject.php#L158
+        $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+        if ($versionInformation->getMajorVersion() >= 13) {
+            $pageUid = $processedData['page']->getId();
+        } else {
+            $pageUid = $processedData['data']['uid'];
+        }
+
+        $contacts = $contactRepository->findByPid($pageUid);
 
         $roles = [];
         foreach ($contacts as $contact) {
