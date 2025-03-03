0297f5285a15cdb56a4f7ab89f83f0ec1c3c53ad
Stefan Bürk <stefan@buerk.tech>
2025-03-03 11:47:28 +0100

    [BUGFIX] Remove PageTS configuration loading
    This extension does not contain any PageTS
    configuration and the code snippet loading
    it within the ext_localconf.php, spamming
    instance logs with
    
    ```
    The "vendor/fgtclb/academic-bite-jobs/Configuration/TsConfig"
    directory does not exist.
    ```
    
    errors. Remove it.

--- packages/fgtclb/academic-bite-jobs/ext_localconf.php
+++ packages/fgtclb/academic-bite-jobs/ext_localconf.php
@@ -1,9 +1,6 @@
 <?php
 
 (static function (): void {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '@import \'EXT:academic_bite_jobs/Configuration/TsConfig/All.tsconfig\''
-    );
 
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'AcademicBiteJobs',
