370fbc5e69f4c3acdf66b60c990ddb0f38be2067
Stefan Bürk <stefan@buerk.tech>
2025-03-04 12:46:38 +0100

    [TASK] Align `ext_emconf.php` with `composer.json`
    Dependency constrainst should be aligned between
    ext_emconf.php and composer.json, otherwise
    it would not be ensured to have the same setup
    between composer and class installations.

--- packages/fgtclb/academic-bite-jobs/ext_emconf.php
+++ packages/fgtclb/academic-bite-jobs/ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'The Academic Bite Jobs extension shows jobs from the B-Ite Job Board.',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-12.4.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
     ],
     'state' => 'stable',
