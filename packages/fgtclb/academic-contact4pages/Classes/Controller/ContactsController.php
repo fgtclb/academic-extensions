a25ff32a942cd13abacb1637c6ad4f072c20a20c
Stefan Bürk <stefan@buerk.tech>
2025-03-31 20:54:25 +0200

    [TASK] Remove TYPO3 v11 content object compat in `contacts4pages`
    The `EXT:academic_contacts4pages` extension still contains some
    TYPO3 v11 compatibility code branches, which are no longer needed.
    
    This change removes the old way to retrieve the content object by
    dropping the conditional TYPO3 v11 branch from the controller. [1]
    
    Makes one PHPStan error ignorePatter obsolete for TYPO3 v13 and
    is removed in the same step.
    
    Used command(s):
    
    ```shell
    Build/Scripts/runTests.sh -t 13 -p 8.2 -s composerUpdate \
    && Build/Scripts/runTests.sh -t 13 -p 8.2 -s phpstanGenerateBaseline
    ```
    
    [1] https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.4/Deprecation-100662-ConfigurationManager-getContentObject.html

--- packages/fgtclb/academic-contact4pages/Classes/Controller/ContactsController.php
+++ packages/fgtclb/academic-contact4pages/Classes/Controller/ContactsController.php
@@ -6,8 +6,6 @@ namespace FGTCLB\AcademicContacts4pages\Controller;
 
 use FGTCLB\AcademicContacts4pages\Domain\Repository\ContactRepository;
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Information\Typo3Version;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 final class ContactsController extends ActionController
@@ -21,16 +19,7 @@ final class ContactsController extends ActionController
 
     public function listAction(): ResponseInterface
     {
-        $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
-
-        // With version TYPO3 v12 the access to the content object renderer has changed
-        // @see https://docs.typo3.org/m/typo3/reference-coreapi/12.4/en-us/ApiOverview/RequestLifeCycle/RequestAttributes/CurrentContentObject.html
-        if ($versionInformation->getMajorVersion() >= 12) {
-            $contentObject = $this->request->getAttribute('currentContentObject');
-        } else {
-            $contentObject = $this->configurationManager->getContentObject();
-        }
-
+        $contentObject = $this->request->getAttribute('currentContentObject');
         $contacts = $this->contactRepository->findByPid($contentObject->data['pid']);
 
         $roles = [];
