8c0887ec845bc0ccb79a835de4ea86a5ed06aead
Stefan Bürk <stefan@buerk.tech>
2025-05-06 22:38:32 +0200

    [TASK] Streamline extbase domain model implementation
    Extbase does not call class constructor for domain models
    when hydrating (thwating) from database, but calls custom
    method `public function initializeObject(): void`, which
    is a long-time known construct.
    
    In older days that was only needed for `ObjectStorage`,
    but became a harder requirement for other types domain
    model properties and specially when custructor property
    promotion is used.
    
    Recommended design of Extbase Domain models boils down
    to:
    
    * Use native typed properties with default assignments as
      long as it is possible.
    * Keep `__construct()` clean and call `initializeObject()`.
    * Use `initializeObject()` to ensure that all properties
      are initialized, mainly when objects like `ObjectStorage`
      needs to be set. Constructor and empty method are added
      to all models with code comment to make clear the use and
      to help on reviews when adding stuff and it needs to be
      added.
    * Do not use `constructor property promotion` for domain
      models, which requires to assign default values for ALL
      constructor promoted parameters despite the assignment
      defined in the constructor.
    * Avoid `: self` for setter.
    
    Note that same logic is applied to extbase based DTO's, but
    not used for not extbase based classes even when they resides
    within `Domain/Model` folders following a bad anti-pattern.
    
    [1] https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ExtensionArchitecture/Extbase/Reference/Domain/Model/Index.html#good-use-initializeobject-for-setup

--- packages/fgtclb/academic-contact4pages/Classes/Domain/Model/Contact.php
+++ packages/fgtclb/academic-contact4pages/Classes/Domain/Model/Contact.php
@@ -10,10 +10,18 @@ use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 class Contact extends AbstractEntity
 {
     protected int $page = 0;
-
     protected ?Contract $contract = null;
-
     protected ?Role $role = null;
+
+    public function __construct()
+    {
+        $this->initializeObject();
+    }
+
+    /**
+     * @link https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ExtensionArchitecture/Extbase/Reference/Domain/Model/Index.html#good-use-initializeobject-for-setup
+     */
+    public function initializeObject(): void {}
 
     public function getPage(): int
     {
