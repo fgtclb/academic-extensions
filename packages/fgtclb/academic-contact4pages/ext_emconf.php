896b9c1204a179792907b2daa50e26a978526a53
Stefan Bürk <stefan@buerk.tech>
2025-05-06 18:54:57 +0200

    [RELEASE] 2.0.1

--- packages/fgtclb/academic-contact4pages/ext_emconf.php
+++ packages/fgtclb/academic-contact4pages/ext_emconf.php
@@ -5,11 +5,11 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Role based relations between profiles and pages',
     'category' => 'fe',
     'state' => 'beta',
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
-            'academic_persons' => '2.0.0',
+            'academic_persons' => '2.0.1',
         ],
         'conflicts' => [],
         'suggests' => [],
