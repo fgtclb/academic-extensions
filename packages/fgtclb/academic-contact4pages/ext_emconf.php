3cbf02de504ddadcb87ef724facc9bdda1add5bf
Stefan Bürk <stefan@buerk.tech>
2025-05-06 21:21:52 +0200

    [TASK] Set version to `2.0.2-dev`

--- packages/fgtclb/academic-contact4pages/ext_emconf.php
+++ packages/fgtclb/academic-contact4pages/ext_emconf.php
@@ -5,11 +5,11 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Role based relations between profiles and pages',
     'category' => 'fe',
     'state' => 'beta',
-    'version' => '2.0.1',
+    'version' => '2.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
-            'academic_persons' => '2.0.1',
+            'academic_persons' => '2.0.2',
         ],
         'conflicts' => [],
         'suggests' => [],
