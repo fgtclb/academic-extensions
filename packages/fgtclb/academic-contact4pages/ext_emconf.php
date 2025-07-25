266833b07637b7d9058664efa3697d00a6e2a9e8
Stefan Bürk <stefan@buerk.tech>
2025-07-25 11:31:44 +0200

    [RELEASE] 1.2.0
    Used command(s):
    
    ```shell
    clear ; USE_VERSION='1.2.0' ; \
    for packageFolder in packages/fgtclb/*; do \
        composerPackageName="$( jq -r '.name' ${packageFolder}/composer.json )" ; \
        extensionKey="$( jq -r '.extra."typo3/cms"."extension-key"' ${packageFolder}/composer.json )" ; \
        packageNames+=("${composerPackageName}") ; \
        echo "${USE_VERSION}" > ${packageFolder}/VERSION ; \
        tailor set-version ${USE_VERSION} --path ${packageFolder} ; \
    done
    ```

--- packages/fgtclb/academic-contact4pages/ext_emconf.php
+++ packages/fgtclb/academic-contact4pages/ext_emconf.php
@@ -7,11 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Role based relations between profiles and pages',
     'category' => 'fe',
     'state' => 'beta',
-    'version' => '1.1.5',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
-            'academic_persons' => '1.1.5',
+            'academic_persons' => '1.2.0',
         ],
         'conflicts' => [],
         'suggests' => [],
