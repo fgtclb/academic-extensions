575b992ddc3394b2bc41c9d1d343da8a2126c59e
Torben Freudewald <torben@web-vision.de>
2026-01-26 17:22:47 +0100

    wip: [TASK] Add ProfileUpdateCommand
    This change introduces a new `ProfileUpdateCommand` for updating
    existing profiles based on frontend user data.
    
    A new native enum with values `Create` and `Update` to distinguish
    between profile operations in events and factories has been created.
    This meant extending the `ChooseProfileFactoryEvent` with an `action`
    property of type `ProfileActionType` and `getAction()` method to allow
    event listeners to distinguish between create and update operations.
    
    Furthermore the `ProfileFactory` was refactored into reusable private
    methods to share them between create and update operations.
    The `ModifyProfileCreateEnvironmentStateBuildContextForFrontendUserEvent`
    renamed to `ModifyProfileCommandEnvironmentStateBuildContextForFrontendUserEvent`
    and extended with the `action` property, to serve as unified event for
    both create and update operations, eliminating code duplication.

---
+++ packages/fgtclb/academic-persons/Classes/Command/UpdateProfilesCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "academic_persons" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace FGTCLB\AcademicPersons\Command;
+
+use FGTCLB\AcademicPersons\Domain\Model\Dto\ProfileUpdateCommandDto;
+use FGTCLB\AcademicPersons\Service\ProfileUpdateCommandService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Command to update existing profiles for frontend users.
+ *
+ * @internal This command is for internal use and may change without notice.
+ */
+final class UpdateProfilesCommand extends Command
+{
+    public function __construct(
+        private readonly ProfileUpdateCommandService $profileUpdateCommandService
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setHelp('This command updates profiles for all frontend users that have a profile.')
+            ->addOption(
+                'exclude-pids',
+                'e',
+                InputOption::VALUE_REQUIRED,
+                'Comma-separated list of PIDs to exclude',
+                null
+            )
+            ->addOption(
+                'include-pids',
+                'i',
+                InputOption::VALUE_REQUIRED,
+                'Comma-separated list of PIDs to include',
+                null
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $dto = new ProfileUpdateCommandDto(
+            includePids: $this->getCommaSeparatedIntegerValueListOptionAsArrayOfIntegerValues($input, 'include-pids'),
+            excludePids: $this->getCommaSeparatedIntegerValueListOptionAsArrayOfIntegerValues($input, 'exclude-pids'),
+        );
+
+        $this->profileUpdateCommandService->execute($dto);
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param string $option
+     * @return int[]
+     */
+    private function getCommaSeparatedIntegerValueListOptionAsArrayOfIntegerValues(InputInterface $input, string $option): array
+    {
+        if ($option === '') {
+            return [];
+        }
+        $valuesStringList = $this->getOptionWithEmptyStringFallback($input, $option);
+        $values = GeneralUtility::intExplode(',', $valuesStringList, true);
+        $values = array_unique($values);
+        $values = array_values($values);
+        return $values;
+    }
+
+    private function getOptionWithEmptyStringFallback(InputInterface $input, string $option): mixed
+    {
+        if ($option === '' || !$input->hasOption($option)) {
+            return '';
+        }
+        return (string)($input->getOption($option)) ?: '';
+    }
+}
