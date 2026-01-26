24f8d45130435f797a7d4ec9c83436606ed549d5
Torben Freudewald <torben@web-vision.de>
2026-01-26 17:22:47 +0100

    [FEATURE] Add ProfileUpdateCommand
    This change introduces a new `ProfileUpdateCommand` for updating
    existing profiles based on frontend user data.
    
    A new native enum with values `Create` and `Update` to distinguish
    between profile operations in events and factories has been created.
    This meant extending the `ChooseProfileFactoryEvent` with an `action`
    property of type `ProfileActionType` and `getAction()` method to allow
    event listeners to distinguish between create and update operations.
    
    Furthermore the `ProfileFactory` was refactored into reusable private
    methods to share them between create and update operations.
    The `ModifyProfileCreateEnvironmentStateBuildContextForFrontendUserEvent`
    renamed to `ModifyProfileCommandEnvironmentStateBuildContextForFrontendUserEvent`
    and extended with the `action` property, to serve as unified event for
    both create and update operations, eliminating code duplication.

--- packages/fgtclb/academic-persons/Configuration/TCA/tx_academicpersons_domain_model_profile_information.php
+++ packages/fgtclb/academic-persons/Configuration/TCA/tx_academicpersons_domain_model_profile_information.php
@@ -159,6 +159,12 @@ $tcaConfiguration = [
                 'type' => 'passthrough',
             ],
         ],
+        'import_identifier' => [
+            'label' => 'LLL:EXT:academic_persons/Resources/Private/Language/locallang_tca.xlf:generic.columns.import_identifier.label',
+            'config' => [
+                'type' => 'passthrough',
+            ],
+        ],
     ],
     'palettes' => [
         'date' => [
