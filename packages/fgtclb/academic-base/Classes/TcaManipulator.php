cf965b9579b3a2081edca8909f99e3d239a6bda5
Torben Freudewald <torben@web-vision.de>
2026-02-09 10:25:39 +0100

    [FEATURE] Import study plan extension from project repository
    To be able to create a study plan for a study program in the backend
    of TYPO3, an academic study plan extension was created. Its goal is
    to be able to create modules for semesters and fill them with useful
    information on the modules' topics. Furthermore modules can be
    assigned to categories, which eventually should be used to filter
    for modules with this category in the frontend.
    
    To achieve this goal a simple content element is created, in which
    semesters and their modules can be maintained with the use of IRRE
    fields. This allows for complete flexibility in the amount and
    sorting of those modules, which naturally is respected in the frontend
    as well.
    
    To fetch the data in a format easily processable for fluid, a
    DataProcessor is added, which gathers all the necessary information
    and provides them in the frontend.
    
    Because categories should have a colour assigned, sys_categories
    can not be used and a new record type for those categories needs to
    be used.
    
    To have a highly accessible frontend on all viewports and for the
    filter functionality JavaScript is used. It handles creating the
    filter buttons so only categories get filters that are actually used
    by the content element. Furthermore it handles the switch between
    mobile and desktop design, as it needs to work a bit different on
    mobile.

--- packages/fgtclb/academic-base/Classes/TcaManipulator.php
+++ packages/fgtclb/academic-base/Classes/TcaManipulator.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace FGTCLB\AcademicBase;
 
+use TYPO3\CMS\Core\Schema\Struct\SelectItem;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
@@ -13,6 +15,64 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  */
 final class TcaManipulator
 {
+    /**
+     * Convenience method so you don't have to deal with strings and arrays and $GLOBALS[TCA] directly that much.
+     *
+     * Adds a new entry to an existing TCA DB table that has a type field configured (via $TCA[$table][ctrl][type])
+     * such as "tt_content" or "pages" tables.
+     *
+     * Takes the $item (label, value[, icon] etc.) and adds the item to the items-array of $TCA[$table]
+     * of the "type" field. The position in the list can be chosen via the $position argument.
+     *
+     * In addition, a type-icon gets registered, and, based on the $item[value], the record type is also added
+     * to $TCA[$table]['types'][$newType], where $showItemList is added as 'showitem' key, as well as $additionalTypeInformation
+     * such as 'columnsOverride' or 'creationOptions'.
+     *
+     * In addition, the $showItemList will receive a 'extended' tab at the very end, so other extensions
+     * that add additional fields, will receive this at the extended tab automatically.
+     *
+     * Can be used in favor of addPlugin() and addTcaSelectItem().
+     *
+     * FOR USE IN files in Configuration/TCA/Overrides/*.php Use in ext_tables.php FILES may break the frontend.
+     *
+     * @param array{label: string, value: int|string|null, description?: string|string[]|null, icon?: string|null, group?: string|null}|SelectItem $item The item to add to the select field
+     * @param string $showItemList A string containing all fields to be used / displayed in this type
+     * @param array<string, mixed> $additionalTypeInformation Additional type information to be added to the type in $TCA[$table]['types']
+     * @param string $position The position in the list where the new item should be added, something like "after:textpic"
+     * @param string $table The table name, defaults to 'tt_content'
+     *
+     * @todo typo3/cms >13.4 Replace usages with `ExtensionManagementUtility::addRecordType()`
+     *       when TYPO3 v12 support is removed and remove this method here.
+     */
+    public function addRecordType(array|SelectItem $item, string $showItemList, array $additionalTypeInformation = [], string $position = '', string $table = 'tt_content'): void
+    {
+        $selectItem = is_array($item) ? SelectItem::fromTcaItemArray($item) : $item;
+        $typeField = $GLOBALS['TCA'][$table]['ctrl']['type'] ?? null;
+        // Throw exception if no type is set
+        if ($typeField === null) {
+            throw new \RuntimeException('Cannot add record type "' . $selectItem->getValue() . '" for TCA table "' . $table . '" without type field defined.', 1725997543);
+        }
+        // Set the type icon as well
+        if ($selectItem->getIcon()) {
+            $GLOBALS['TCA'][$table]['ctrl']['typeicon_classes'][$selectItem->getValue()] = $selectItem->getIcon();
+        }
+        if (!$selectItem->hasGroup()) {
+            $selectItem = $selectItem->withGroup('default');
+        }
+        $relativeInformation = GeneralUtility::trimExplode(':', $position, true, 2);
+        ExtensionManagementUtility::addTcaSelectItem($table, $typeField, $selectItem->toArray(), $relativeInformation[0] ?? '', $relativeInformation[1] ?? '');
+        $showItemList = trim($showItemList, ', ');
+        // Add the extended tab if not already added manually at the very end.
+        if ($showItemList !== '' && !str_contains($showItemList, '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended')) {
+            $showItemList .= ',--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended';
+        }
+        if ($showItemList !== '') {
+            $showItemList .= ',';
+        }
+        $additionalTypeInformation['showitem'] = $showItemList;
+        $GLOBALS['TCA'][$table]['types'][$selectItem->getValue()] = $additionalTypeInformation;
+    }
+
     /**
      * Add `$definitionToAdd` as last item to the general tab for page types.
      *
