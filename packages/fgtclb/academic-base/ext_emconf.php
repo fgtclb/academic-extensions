6d6d7a380a31823a8fb38cd6fe1b7c12430cea1f
Stefan Bürk <stefan@buerk.tech>
2026-05-05 16:48:41 +0200

    [TASK] ACE-228: Normalize `ext_emconf.php`
    This changes uses the `ext_emconf.php` normalization of the
    upcoming tool `packwright` to all of the split- and test
    extension files in a good shape.
    
    Used command(s):
    
    ```bash
    pkw-dev extemconf:normalize -t 12 -- packages/**/ext_emconf.php && \
    pkw-dev extemconf:set \
      --ext-version=2.3.2 \
      --author-company='FGTCLB GmbH' \
      --author-email='hello@fgtclb.com' \
      -- packages/**/ext_emconf.php
    ```
    
    > [!NOTE]
    > `pkw (packwraight)` not realease in public yet. It's the upcoming
    > TYPO3 extension release took with a couple of helpers. Currently
    > only `ext_emconf.php` validation, normalization, value setting and
    > constraints handling is availalbe and the reason that the development
    > version has been used.
    
    This is done as preparation for the next release.

--- packages/fgtclb/academic-base/ext_emconf.php
+++ packages/fgtclb/academic-base/ext_emconf.php
@@ -1,18 +1,18 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
+    'title' => 'FGTCLB: Academic Base',
+    'description' => 'Base functionality accross academic extensions.',
+    'version' => '2.3.2',
+    'category' => 'misc',
+    'state' => 'beta',
     'author' => 'FGTCLB',
-    'author_company' => 'FGTCLB GmbH',
     'author_email' => 'hello@fgtclb.com',
-    'category' => 'fe,be',
+    'author_company' => 'FGTCLB GmbH',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.22-13.4.99',
             'backend' => '12.4.22-13.4.99',
         ],
     ],
-    'description' => 'Base functionality accross academic extensions.',
-    'state' => 'beta',
-    'title' => 'FGTCLB: Academic Base',
-    'version' => '2.3.2',
 ];
